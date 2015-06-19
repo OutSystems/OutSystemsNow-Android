c22b092f1288bb53c7a0ba35e4ff955a7764b260
Luis Silva <luis.silva@outsystems.com>
2015-06-19 18:06:08 +0100

    Fixed: database version

--- OutSytems/platforms/android/src/com/outsystems/android/core/DatabaseHandler.java
+++ OutSytems/platforms/android/src/com/outsystems/android/core/DatabaseHandler.java
@@ -38,7 +38,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     // All Static variables
     // Database Version
-    private static final int DATABASE_VERSION = 4;
+    private static final int DATABASE_VERSION = 5;
 
     // Database Name
     private static final String DATABASE_NAME = "hubManager";
