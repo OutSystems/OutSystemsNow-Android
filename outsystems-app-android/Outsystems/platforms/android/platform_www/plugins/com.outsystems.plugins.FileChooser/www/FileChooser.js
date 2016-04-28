98a917599bb0381eddbd9f328586c2db41873397
Luis Silva <luis.silva@outsystems.com>
2016-04-28 11:46:55 +0100

    Added missing plugin Cordova-Plugin-Network-Information

---
+++ outsystems-app-android/Outsystems/platforms/android/platform_www/plugins/com.outsystems.plugins.FileChooser/www/FileChooser.js
@@ -0,0 +1,23 @@
+cordova.define("com.outsystems.plugins.FileChooser.FileChooser", function(require, exports, module) { /**
+ * OutSystems R&D
+ */
+ (function(cordova){
+    var FileChooser = function() {
+
+    };
+
+    FileChooser.prototype.open = function(params, success, fail) {
+        return cordova.exec(function(args) {
+            success(args);
+        }, function(args) {
+            fail(args);
+        }, 'FileChooser', 'open', [params||{}]);
+    };
+
+    window.filechooser = new FileChooser();
+    
+    // backwards compatibility
+    window.plugins = window.plugins || {};
+    window.plugins.filechooser = window.filechooser;
+})(window.PhoneGap || window.Cordova || window.cordova);
+});
